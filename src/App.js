2c8c712b8f38164ed161f8930ccdd4b0718b3c35
Samrat <paudelsamrat27@gmail.com>
2024-12-15 13:30:51 +0545

    firt commit dj

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <div>
+    <h1>Start building Mishra DJ</h1>
+    <Navbar />
+    {/*  make components and render here -> hero section, about me , contact etc. */}
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+          <h1>Here comes logo and menu items</h1>
+        </div>
+      )
+}
+
+export default Navbar
